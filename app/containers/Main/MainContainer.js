48e1144dbaef90d3fdb28c59695b5b84f8da0e56
Bolaji Adeyemi <Bolaji.Adeyemi@mindera.com>
2017-10-11 16:37:51 +0100

    Add Authenticate

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import CreateClass from 'create-react-class';
+import { Navigation } from 'components'
+import { container, innerContainer } from './styles.css';
 
 const MainContainer = CreateClass({
   render () {
     return (
-     <div>
+     <div className={container}>
+     <Navigation isAuthed={false}/>
+     <div className={innerContainer}>
      {this.props.children}
+     </div>
      </div>
       )
   },
---
+++ app/helpers/auth.js
@@ -0,0 +1,11 @@
+export default function auth() {
+    return new Promose((resolve, reject) => {
+        setTimeout(() => {
+             resolve({
+              name:'Bolaji Adeyemi',
+              avatar:'https://secure.gravatar.com/avatar/ef114bf91fce8490912a9d6ea8e911b2',
+              uid:'bolajiadeyemi'
+             })
+        }, 2000)
+    })
+}
