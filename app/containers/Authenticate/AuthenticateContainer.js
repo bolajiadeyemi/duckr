ded3975e2dd0754ea9f5e45d69d3d2f575f40db8
Bolaji Adeyemi <Bolaji.Adeyemi@mindera.com>
2017-10-24 06:32:23 +0100

    Add middleware

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Authenticate } from 'components';
-import auth from 'helpers/auth';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as userActionCreators from 'redux/modules/users';
 
 const AuthenticateContainer = React.createClass({
+   propTypes:{
+   isFetching: PropTypes.bool.isRequired,
+   error: PropTypes.string.isRequired,
+   fetchAndHandleAuthedUser: PropTypes.func.isRequired
 
+   },
   handleAuth (){
-    auth().then((user) => {
-      console.log('Authed User',user);
-    })
+   this.props.fetchAndHandleAuthedUser();
   },
   render () {
+    console.log(this.props);
     return (
     <Authenticate
-    isFetching={false}
-    error=''
+    isFetching={this.props.isFetching}
+    error={this.props.error}
     onAuth={this.handleAuth}
     />
     )
   },
 })
-export default AuthenticateContainer
+
+function mapStateToProps(state){
+  console.log(state);
+  const {isFetching, error} = state
+  return {
+  isFetching,
+  error
+ }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators(userActionCreators, dispatch);
+}
+export default connect(
+// (state) => ({isFetching: state.isFetching, error: state.error}),
+mapStateToProps,
+(dispatch) => bindActionCreators(userActionCreators, dispatch)
+  )(AuthenticateContainer)
--- app/index.js
+++ app/index.js
@@ -1,8 +1,21 @@
-import ReactDOM from 'react-dom'
-import routes from './config/routes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import routes from './config/routes';
+import { createStore, applyMiddleware } from 'redux';
+import users from 'redux/modules/users';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(
+const store = createStore(users, applyMiddleware(thunk));
+
+/*ReactDOM.render(
   routes,
   document.getElementById('app')
-  )
+  )*/
 
+ReactDOM.render(
+  <Provider store={store}>
+  {routes}
+  </Provider>,
+    document.getElementById('app')
+  )
